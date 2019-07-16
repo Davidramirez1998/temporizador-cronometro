ae355c2d17bc681ccd552d36f32e10671565b648
Davidramirez1998 <52727273+Davidramirez1998@users.noreply.github.com>
2019-07-15 21:11:05 -0500

    Add files via upload

---
+++ cliente_temporizador_cronometro.py
@@ -0,0 +1,27 @@
+from temporizador import Temporizador
+from time import sleep
+ 
+t = Temporizador()
+
+t.iniciar([0,1,15])
+
+
+while True:
+    tiempo = t.mostrar_tiempo()
+    print tiempo
+    sleep(0.1)
+    if tiempo == "00 : 00 : 00":
+        break
+    t.retroceder()
+    
+while True:
+    tiempo = t.mostrar_tiempo ()
+    print tiempo
+    sleep (0.1)
+    if tiempo == "00 : 01 : 15":
+        break
+    t.avanzar ()
+    
+
+
+
